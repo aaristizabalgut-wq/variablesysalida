3f56effa268facb5296103071b93993a120eac50
Ana <aaristizabalgut@gmail.com>
2026-02-27 10:18:17 -0500

    puse el .vs en txt

---
+++ variablesysalida/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+class descripcion
+{
+    static void Main(string[] args)
+    {
+        string nombre = "Juana";
+        int edad = 13;
+        float estatura = 1.40f;
+
+        Console.WriteLine($"Holaa {nombre}, tienes {edad} años y mides: {estatura:F2} ");
+    }
+
+}
+
